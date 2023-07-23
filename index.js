53cdd92ceb9db961ae9b8646658c7c93eeef48dc
Nishan42069 <109575272+Nishan42069@users.noreply.github.com>
2023-07-23 12:05:46 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const sideMenu = document.querySelector("aside");
+const menuBtn = document.querySelector("#menu-btn");
+const closeBtn = document.querySelector("#close-btn");
+const themeToggler = document.querySelector(".theme-Toggler");
+
+menuBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'block';
+})
+
+closeBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'none';
+})
+
